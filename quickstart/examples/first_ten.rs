b0fe66a366da432e861ae70fad7d47fd85b74669
Alex Wang <hpwzz20@gmail.com>
2025-10-12 19:28:08 -0700

    comments

--- quickstart/examples/first_ten.rs
+++ quickstart/examples/first_ten.rs
@@ -3,11 +3,14 @@ use hydro_deploy::Deployment;
 #[tokio::main]
 async fn main() {
     let mut deployment = Deployment::new();
+    // create flowbuilder -> store entire dataflow prog & manage its compilation
 
     let flow = hydro_lang::compile::builder::FlowBuilder::new();
     let process = flow.process();
     hydro_template::first_ten::first_ten(&process);
 
+    // flow.deploy() -> provisions dataflow prog on target machine
+    // returns _nodes, a struct w/ handles to instantiated machines
     let _nodes = flow
         .with_process(&process, deployment.Localhost())
         .deploy(&mut deployment);
--- quickstart/examples/first_ten_cluster.rs
+++ quickstart/examples/first_ten_cluster.rs
@@ -4,13 +4,11 @@ use hydro_deploy::Deployment;
 async fn main() {
     let mut deployment = Deployment::new(); // init hydro deployment
 
-    // create flowbuilder -> store entire dataflow prog & manage its compilation
     let flow = hydro_lang::compile::builder::FlowBuilder::new();
     let leader = flow.process();
     let workers = flow.cluster();
     hydro_template::first_ten_cluster::first_ten_cluster(&leader, &workers);
 
-    // flow.deploy() -> provisions dataflow prog on target machine
     let _nodes = flow
         .with_process(&leader, deployment.Localhost())
         .with_cluster(&workers, vec![deployment.Localhost(); 4])
